9e11ed7364a38f59eb6822262d9dc06a5466f092
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2016-04-07 21:28:11 -0400

    Enabled admin feature

--- shortlink/shorturls/admin.py
+++ shortlink/shorturls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Shortlink
+
+class LinkAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Shortlink, LinkAdmin)
